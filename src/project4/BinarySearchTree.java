342fa2f7e42b55461b947e7ecd5af5e1b6eea85b
vonclites <alamos52@gmail.com>
2017-11-16 12:16:41 -0500

    updating instructions and tests

---
+++ src/project4/BinarySearchTree.java
@@ -0,0 +1,60 @@
+/*
+ * Name: 
+ * Instructor:
+ * Date:
+ * Assignment: Project 4 (Binary Search Trees)
+ * Purpose:
+ */
+package project4;
+
+public class BinarySearchTree<E extends Comparable<E>> extends BinaryTree<E> {
+	
+	public BinarySearchTree() {
+		super();
+	}
+
+	public BinarySearchTree(E root) {
+		super(root);
+	}
+	
+	public BinarySearchTree(E root, BinarySearchTree<E> left, BinarySearchTree<E> right){
+		super(root, left, right);
+	}
+	
+	/**
+     * Inserts item where it belongs in the tree.
+     *
+     * @param item The item to be inserted
+     * @return true If the item is inserted, false if the item was already in
+     * the tree.
+     */
+	public boolean add(E item) {
+		// TODO Implement me.
+		return false;
+	}
+	
+	// TODO: Fill in the following block comment.
+	/**
+     * 
+     *
+     * @param target 
+     * @return
+     */
+	public boolean contains(E target) {
+		// TODO Implement me.
+		return false;
+	}
+	
+	// TODO: Fill in the following block comment.
+	/**
+     * 
+     *
+     * @param target 
+     * @return 
+     */
+	public boolean remove(E target) {
+		// TODO Implement me.
+		return false;
+	}
+
+}
