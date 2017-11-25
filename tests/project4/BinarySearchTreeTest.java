192e46cd37c79dc499f6fed3dbd1d12882aa13d5
Domenick Poster <dposter@mix.wvu.edu>
2017-11-25 15:43:02 -0500

    Additional test cases for remove

--- tests/project4/BinarySearchTreeTest.java
+++ tests/project4/BinarySearchTreeTest.java
@@ -106,6 +106,9 @@ public class BinarySearchTreeTest {
      *       /    \       /    \
      *      /      \     /      \
      *    cow    jack   milked  that
+     *                   / \
+     *                  /   \
+     *                lit   nap
 	 */
 	
 	@Test
@@ -117,16 +120,33 @@ public class BinarySearchTreeTest {
 		Node<String> jack = new Node<String>("jack");
 		Node<String> rat = new Node<String>("rat");
 		Node<String> milked = new Node<String>("milked");
+		Node<String> lit = new Node<String>("lit");
+		Node<String> nap = new Node<String>("nap");
 		Node<String> that = new Node<String>("that");
+		
+		bst.root = lay;
+		lay.left = house; lay.right = rat;
+		
+		assertTrue(bst.remove("lay"));
+		String[] expecteds = { "house", "rat" };
+		String[] actuals = new String[2];
+		actuals = bst.inOrderTraverse().toArray(actuals);
+		assertArrayEquals(expecteds, actuals);
+		
+		bst = new BinarySearchTree<String>();
+		lay = new Node<String>("lay");
+		house = new Node<String>("house");
 		
 		bst.root = lay;
 		lay.left = house; lay.right = rat;
 		house.left = cow; house.right = jack;
 		rat.left = milked; rat.right = that;
+		milked.left = lit; milked.right = nap;
 		
 		assertFalse(bst.remove("jill"));
-		String[] expecteds1 = {"cow", "house", "jack", "lay", "milked", "rat", "that"};
-		String[] actuals = new String[7];
+		String[] expecteds1 = { "cow", "house", "jack", "lay", "lit", "milked",
+				"nap", "rat", "that" };
+		actuals = new String[9];
 		actuals = bst.inOrderTraverse().toArray(actuals);
 		assertArrayEquals(expecteds1, actuals);
 		
@@ -136,33 +156,35 @@ public class BinarySearchTreeTest {
 		
 		
 		assertTrue(bst.remove("jack"));
-		String[] expecteds2 = {"cow", "house", "lay", "milked", "rat", "that"};
-		actuals = new String[6];
+		String[] expecteds2 = { "cow", "house", "lay", "lit", "milked", "nap",
+				"rat", "that" };
+		actuals = new String[8];
 		actuals = bst.inOrderTraverse().toArray(actuals);
 		assertArrayEquals(expecteds2, actuals);
 		
 		assertTrue(bst.remove("house"));
-		String[] expecteds3 = {"cow", "lay", "milked", "rat", "that"};
-		actuals = new String[5];
+		String[] expecteds3 = { "cow", "lay", "lit", "milked", "nap", "rat",
+				"that" };
+		actuals = new String[7];
 		actuals = bst.inOrderTraverse().toArray(actuals);
 		assertArrayEquals(expecteds3, actuals);
 		
 		assertTrue(bst.remove("cow"));
-		String[] expecteds4 = {"lay", "milked", "rat", "that"};
-		actuals = new String[4];
+		String[] expecteds4 = { "lay", "lit", "milked", "nap", "rat", "that" };
+		actuals = new String[6];
 		actuals = bst.inOrderTraverse().toArray(actuals);
 		assertArrayEquals(expecteds4, actuals);
 		
 		assertTrue(bst.remove("rat"));
-		String[] expecteds5 = {"lay", "milked", "that"};
-		actuals = new String[3];
+		String[] expecteds5 = { "lay", "lit", "milked", "nap", "that" };
+		actuals = new String[5];
 		actuals = bst.inOrderTraverse().toArray(actuals);
 		assertArrayEquals(expecteds5, actuals);
 		
 		
 		assertTrue(bst.remove("milked"));
-		String[] expecteds6 = {"lay", "that"};
-		actuals = new String[2];
+		String[] expecteds6 = { "lay", "lit", "nap", "that" };
+		actuals = new String[4];
 		actuals = bst.inOrderTraverse().toArray(actuals);
 		assertArrayEquals(expecteds6, actuals);
 	}
